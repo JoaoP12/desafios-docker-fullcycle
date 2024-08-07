eb78962ffbb42dfea329a939c856a21dc6605b00
Joao Lopes <joao.lopes@exati.com.br>
2024-08-07 18:13:21 -0400

    Finalizada aplicação node + mysql

---
+++ desafio-nginx-node/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+const config = {
+  host: "mysql",
+  user: "root",
+  password: "jp123",
+  database: "challenge",
+};
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection(config);
+
+const insertSQL = `INSERT INTO people(name) VALUES('JP');`;
+connection.query(insertSQL, function () {});
+
+app.get("/", (req, res) => {
+  connection.query(`SELECT name FROM people;`, function (err, results) {
+    if (results) {
+      const names = results.map((result) => result.name).join("<br />- ");
+      res.send(`<h1>Full Cycle Rocks!</h1><br />- ${names}`);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
